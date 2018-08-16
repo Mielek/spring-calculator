a6e5a435b6060cc03205210e2081057af0ae8335
Rafal Mielowski <mielowski.rafal@gmail.com>
2018-08-16 15:40:56 +0200

    standardization of method names in tests

--- src/test/java/com/mielowski/calculator/operation/AdditionOperationTest.java
+++ src/test/java/com/mielowski/calculator/operation/AdditionOperationTest.java
@@ -19,7 +19,7 @@ public class AdditionOperationTest {
             "10, -20, -10",
             "0.5, -1, -0.5"
     })
-    public void addXtoYEqualsZ(BigDecimal x, BigDecimal y, BigDecimal z) {
+    public void xAddedToYEqualsZ(BigDecimal x, BigDecimal y, BigDecimal z) {
         BigDecimal result = new AdditionOperation(ValueOperation.of(x), ValueOperation.of(y)).result();
 
         assertThat(result).isEqualTo(z);
--- src/test/java/com/mielowski/calculator/operation/SubtractionOperationTest.java
+++ src/test/java/com/mielowski/calculator/operation/SubtractionOperationTest.java
@@ -12,7 +12,7 @@ public class SubtractionOperationTest {
     @DisplayName("Subtraction of values:")
     @ParameterizedTest(name = "{0} - {1} = {2}")
     @CsvSource({"1, 1, 0", "2, 1, 1", "100.05, 0.05, 100.00", "25, -25, 50"})
-    public void subtractXWithYEqualsZ(BigDecimal x, BigDecimal y, BigDecimal z){
+    public void xSubtractedByYEqualsZ(BigDecimal x, BigDecimal y, BigDecimal z){
         BigDecimal result = new SubtractionOperation(ValueOperation.of(x), ValueOperation.of(y)).result();
 
         assertThat(result).isEqualTo(z);
