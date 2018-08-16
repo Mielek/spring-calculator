ad30a60b5e855d0167ed8e4b24b582e3b9590859
Rafal Mielowski <mielowski.rafal@gmail.com>
2018-08-16 21:02:07 +0200

    Refactored parseExpression to be more intuitive

--- src/main/java/com/mielowski/calculator/ExpressionParser.java
+++ src/main/java/com/mielowski/calculator/ExpressionParser.java
@@ -3,6 +3,8 @@ package com.mielowski.calculator;
 
 import com.mielowski.calculator.expressions.*;
 
+import java.util.stream.IntStream;
+
 /**
  * Based on <a href="https://stackoverflow.com/a/26227947">Thread on StackOverflow</a>
  */
@@ -11,17 +13,20 @@ public class ExpressionParser {
     private String expression;
     private int currentPosition = -1;
     private int currentCharacter;
+    private int lastOperationCharacter;
 
     public ExpressionParser(String expression) {
-        this.expression = expression;
+        this.expression = expression.trim().toLowerCase();
     }
 
 
     public Expression parse() {
+        if (expression.isEmpty())
+            throw new RuntimeException("Expression is empty");
         nextChar();
         Expression x = parseExpression();
         if (currentPosition < expression.length())
-            throw new RuntimeException("Unexpected: " + (char) currentCharacter);
+            throw new RuntimeException("Unexpected expression ending " + expression.substring(currentPosition));
         return x;
     }
 
@@ -33,17 +38,41 @@ public class ExpressionParser {
 
     private Expression parseExpression() {
         Expression x = parseTerm();
-        for (;;) {
-            if      (eat('+')) x = AdditionExpression.of(x, parseTerm()); // addition
-            else if (eat('-')) x = SubtractionExpression.of(x, parseTerm()); // subtraction
-            else return x;
+        while (isNextOperation('+', '-')) {
+            x = createTwoValueExpression(x, parseTerm());
         }
+        return x;
+    }
+
+    private Expression createTwoValueExpression(Expression left, Expression right) {
+        switch (lastOperationCharacter) {
+            case '+':
+                return AdditionExpression.of(left, right);
+            case '-':
+                return SubtractionExpression.of(left, right);
+            default:
+                throw new RuntimeException("Unknown operation under char: " + (char) lastOperationCharacter);
+        }
+    }
+
+    private boolean isNextOperation(int... operations) {
+        skipWhiteCharacters();
+        if (IntStream.of(operations).anyMatch(value -> value == currentCharacter)) {
+            lastOperationCharacter = currentCharacter;
+            nextChar();
+            return true;
+        }
+        return false;
+    }
+
+    private void skipWhiteCharacters() {
+        while (currentCharacter == ' ') nextChar();
     }
 
     private Expression parseTerm() {
         Expression x = parseFactor();
-        for (;;) {
-            if      (eat('*')) x = MultiplyExpression.of(x, parseFactor()); // multiplication
+        for (; ; ) {
+            if (eat('*')) x = MultiplyExpression.of(x, parseFactor()); // multiplication
             else if (eat('/')) x = DivisionExpression.of(x, parseFactor()); // division
             else return x;
         }
@@ -57,15 +86,15 @@ public class ExpressionParser {
         int startPos = this.currentPosition;
         if (eat('(')) { // parentheses
             x = parseExpression();
-            if(!eat(')'))
+            if (!eat(')'))
                 throw new RuntimeException("No ending parenthesis");
         } else if (eat('[')) { // parentheses
             x = parseExpression();
-            if(!eat(']'))
+            if (!eat(']'))
                 throw new RuntimeException("No ending parenthesis");
         } else if (eat('{')) { // parentheses
             x = parseExpression();
-            if(!eat('}'))
+            if (!eat('}'))
                 throw new RuntimeException("No ending parenthesis");
         } else if ((currentCharacter >= '0' && currentCharacter <= '9') || currentCharacter == '.') { // numbers
             while ((currentCharacter >= '0' && currentCharacter <= '9') || currentCharacter == '.') nextChar();
@@ -75,7 +104,7 @@ public class ExpressionParser {
             String func = expression.substring(startPos, this.currentPosition);
             x = parseFactor();
             if (func.equals("sqrt")) x = SquareExpression.of(x);
-            else if(func.equals("root")) x = SquareRootExpression.of(x);
+            else if (func.equals("root")) x = SquareRootExpression.of(x);
             else throw new RuntimeException("Unknown function: " + func);
         } else {
             throw new RuntimeException("Unexpected: " + (char) currentCharacter);
@@ -84,8 +113,8 @@ public class ExpressionParser {
         return x;
     }
 
-    private void nextChar() {
-        currentCharacter = (++currentPosition < expression.length()) ? expression.charAt(currentPosition) : -1;
+    private int nextChar() {
+        return currentCharacter = (++currentPosition < expression.length()) ? expression.charAt(currentPosition) : -1;
     }
 
     private boolean eat(int charToEat) {
