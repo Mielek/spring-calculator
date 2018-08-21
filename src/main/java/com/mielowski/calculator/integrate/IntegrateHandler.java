c705f9a1b3ded415ee8d42f2bd276d750e0e5f63
Rafal Mielowski <mielowski.rafal@gmail.com>
2018-08-22 01:35:06 +0200

    Better thread handling with ForkJoinPool and stream api

--- src/main/java/com/mielowski/calculator/integrate/IntegrateHandler.java
+++ src/main/java/com/mielowski/calculator/integrate/IntegrateHandler.java
@@ -2,10 +2,9 @@ package com.mielowski.calculator.integrate;
 
 import com.mielowski.calculator.core.CommandHandler;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Stream;
 
 public class IntegrateHandler implements CommandHandler<IntegrateCommand, Double> {
     @Override
@@ -13,39 +12,44 @@ public class IntegrateHandler implements CommandHandler<IntegrateCommand, Double
         checkSplits(command.getSplits());
         ExecutorService executor = getExecutor(command.getThreads());
         try {
-            return createSplit(command).stream()
-                    .map(executor::submit)
-                    .map(doubleFuture -> {
-                        try{
-                            return doubleFuture.get();
-                        } catch (Exception e){
-                            throw new IntegrateException("Job failure", e);
-                        }
-                    })
-                    .reduce(0.0, (left, right) -> left+right);
-        }finally {
+            return executor.submit(() ->
+                    createSplit(command)
+                            .parallel()
+                            .map(range -> Math.exp(range.stop) - Math.exp(range.start))
+                            .reduce(0.0, (left, right) -> left + right))
+                    .get();
+        } catch (Exception e) {
+            throw new IntegrateException("Job failure", e);
+        } finally {
             executor.shutdown();
         }
     }
 
     private void checkSplits(int splits) {
-        if(splits<=0) throw new IntegrateException("Split must be at least 1");
+        if (splits <= 0) throw new IntegrateException("Split must be at least 1");
     }
 
     private ExecutorService getExecutor(int threads) {
-        if(threads<=0) throw new IntegrateException("Threads number must be at least 1");
-        return Executors.newFixedThreadPool(threads);
+        if (threads <= 0) throw new IntegrateException("Threads number must be at least 1");
+        return new ForkJoinPool(threads);
+
     }
 
-    private List<IntegrateJob> createSplit(IntegrateCommand command) {
-        List<IntegrateJob> integralJobs = new ArrayList<>();
+    private Stream<Range> createSplit(IntegrateCommand command) {
         double splitRage = (command.getEndRange() - command.getStartRange()) / command.getSplits();
-        double start = command.getStartRange();
-        for(int i =0; i<command.getSplits(); ++i){
-            double end = start + splitRage;
-            integralJobs.add(new IntegrateJob(start, end));
-            start = end;
+        return Stream.iterate(
+                new Range(command.getStartRange(), splitRage),
+                range -> range.stop + splitRage < command.getEndRange(),
+                range -> new Range(range.stop, range.stop + splitRage));
+    }
+
+    private static class Range {
+        double start;
+        double stop;
+
+        public Range(double start, double stop) {
+            this.start = start;
+            this.stop = stop;
         }
-        return integralJobs;
     }
 }
